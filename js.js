7b96a611bb4c6cc613514e6303644929580110ac
unfold4 <ayushv4y@gmail.com>
2026-01-14 08:20:02 -0600

    fixed some bugs

--- js.js
+++ js.js
@@ -8,6 +8,7 @@ function multiply(a,b){
   return a*b;
 }
 function divide(a,b){
+  if(b===0) return "Cannot divide by zero";
   return a/b;
 }
 function power(a,b){
@@ -15,21 +16,23 @@ function power(a,b){
 }
 
 
-  function operate(a,b,op){
+function operate(a,b,op){
     if(op==='+') return sum(a,b);
     if(op==='-') return subtract(a,b);
     if(op==='*') return multiply(a,b);
     if(op==='/') return divide(a,b);
     if(op==='^') return power(a,b);
-  }
+}
 
 
 function clear(){
   v="";
   a=undefined;
   b=undefined;
+  lastEnteredNum=undefined;
   mainDisplay.innerText="";
   bufferDisplay.innerText="";
+  selectOperator=false;
 }
 
 function deletee(){
@@ -37,17 +40,41 @@ function deletee(){
 }
 
 let v="";
+let lastEnteredNum;
+selectOperator=false;
 
 let mainDisplay=document.querySelector(".display .main");
 let bufferDisplay=document.querySelector(".display .buffer");
 
 
+
+
+
+
+
+
+
+
+
+// trying to add keyboard clicks 
+// document.addEventListener("keydown",(e)=>{
+//   let numbers="1234567890";
+//   let operators="/+-^*";
+
+//   if(numbers.includes(e.key)){
+//     document.querySelector(e.key).click();
+
+
+//   }
+// })
+
+
 // press '=' event listener 
 document.querySelector(".equals").addEventListener("click",()=>{
-  b=parseInt(v);
-  bufferDisplay.innerText+=b+'=';
-  mainDisplay.innerHTML=operate(a,b,op)
 
+  b=parseInt(lastEnteredNum);
+  bufferDisplay.innerText+=b+'=';
+  mainDisplay.innerHTML=operate(a,b,op);
 
 })
 
@@ -57,7 +84,9 @@ document.querySelector(".equals").addEventListener("click",()=>{
 
     if(e.target.classList.contains("num")) {
       v+=e.target.innerText;
+      lastEnteredNum=v;
       mainDisplay.innerText=v;
+      selectOperator=true;
 
 
     }});
@@ -74,18 +103,21 @@ document.querySelector(".equals").addEventListener("click",()=>{
 
   // function event listener
   document.querySelector(".buttons .sec2").addEventListener("click",(e)=>{
+  if(e.target.innerText!==op && !selectOperator) {op=e.target.innerText;
+    let x=bufferDisplay.innerText;
+    bufferDisplay.innerText=x.slice(0,x.length-1)+op;
 
-   if(mainDisplay.innerText!=="")
-    
-    {
+  }
+
+   if(selectOperator){
     if(a===undefined){
       op=e.target.innerText;
       bufferDisplay.innerText=v+op;
-      a=parseInt(v);
-      v=""
+      a=parseInt(lastEnteredNum);
+      v="";
     }
     else{
-      b=parseInt(v);
+      b=parseInt(lastEnteredNum);
       a=operate(a,b,op);
       op=e.target.innerText;
       bufferDisplay.innerText=`${a}${op}`;
@@ -93,6 +125,7 @@ document.querySelector(".equals").addEventListener("click",()=>{
       v="";
     }
   }
+  selectOperator=false;
   })
   
 
@@ -101,3 +134,13 @@ document.querySelector(".equals").addEventListener("click",()=>{
   let b ;
   let op;
 
+
+
+  //currently trying to do operator multiple clicks and failing!
+
+  //exceptions to deal with :
+  // - equal after equal
+  // - 2= , 2+ = etc
+  // - make delete button functional 
+  // - make keyboard to press
+  // - make decimal button functional;
